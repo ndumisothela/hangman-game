83f10f872238b1638bf590b0b620d001d13e8bbf
Ndumiso Thela <ndumisothela480@gmail.com>
2023-08-01 23:31:57 +0200

    homepage and comments

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import Hangman from "./Components/Hangman";
 import Header from "./Components/Header";
 import { Routes, Route } from "react-router-dom";
 import Help from "./Components/Help";
+import Homepage from "./Components/Homepage";
 
 const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Route exact path="/" element={<Hangman />} />
+        <Route exact path="/" element={<Homepage />} />
+      </Routes>
+      <Routes>
+        <Route exact path="game" element={<Hangman />} />
       </Routes>
       <Routes>
         <Route path="/help" element={<Help />} />
